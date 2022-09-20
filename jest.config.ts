6fe8ac2d2310d6dfbcf3a38f616e9f52d74d2db8
ElMarto <martin@mata.com>
2022-09-19 21:18:30 -0300

    Configura Jest para test unitarios

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+      diagnostics: false,
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  roots: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/*.test.(ts|js)'],
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts', // include all files, even files that have no tests yet (or are never called)
+  ],
+};
